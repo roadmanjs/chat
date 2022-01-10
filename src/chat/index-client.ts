0ed6a550089ab1d76476bfee0e68eb4866a12e6a
cmuhoza <ceddymuhoza@gmail.com>
2022-01-09 21:34:50 -0500

    shelljs and final exports

---
+++ src/chat/index-client.ts
@@ -0,0 +1,7 @@
+/**
+ * ONLY MEANT FOR CLIENTS, but can also work on server-side.
+ */
+export * from './fragments';
+export * from './mutations';
+export * from './query';
+export * from './subscriptions';
