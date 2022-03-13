474d94eced90f3fd5264c19c9b0d8db987046ce3
cmuhoza <ceddymuhoza@gmail.com>
2022-03-12 19:19:45 -0500

    Minor 0.1

--- src/chat/methods/ChatConvo.test.ts
+++ src/chat/methods/ChatConvo.test.ts
@@ -11,22 +11,22 @@ before((done) => {
 });
 
 describe("ChatConvo", () => {
-  // it("Create convo", async () => {
-  //   const owner = "1c01d85f-b811-44b1-8a4f-bef8030bf265";
-  //   const otherUser = "99bc43ba-02ab-4394-b48b-49a39a95443c";
-  //   const members = [owner, otherUser];
+  it("Create convo", async () => {
+    const owner = "1c01d85f-b811-44b1-8a4f-bef8030bf265";
+    const otherUser = "99bc43ba-02ab-4394-b48b-49a39a95443c";
+    const members = [owner, otherUser];
 
-  //   const chatConvo = {
-  //       members,
-  //       group: false,
+    const chatConvo = {
+        members,
+        group: false,
 
-  //   };
+    };
 
-  //   const createdConvo = await createChatConvoType(chatConvo);
+    const createdConvo = await createChatConvoType(chatConvo);
     
-  //   console.log("Created", createdConvo);
-  //   expect(createdConvo).to.not.be.empty;
-  // });
+    console.log("Created", createdConvo);
+    expect(createdConvo).to.not.be.empty;
+  });
 
   it("Remove read count", async () => {
     const owner = "1c01d85f-b811-44b1-8a4f-bef8030bf265";
